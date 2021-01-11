628cb8a9d1bd3069ac5dd752006fc243777d7a01
jesusdja <jesusdja@smartdream.es>
2021-01-11 11:09:28 -0400

    inicializar firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,11 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'App.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -23,7 +27,7 @@ class MyApp extends StatelessWidget {
           const Locale('es', 'ES'), // English
           const Locale('en', 'US'), // English
         ],
-        home: AppFR(),
+        home: App(),
         theme: ThemeData(
           fontFamily: 'helveticaneue',
         )
@@ -32,27 +36,41 @@ class MyApp extends StatelessWidget {
 }
 
 class AppFR extends StatelessWidget {
-  // Create the initialization Future outside of `build`:
+
   final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      // Initialize FlutterFire:
-      future: _initialization,
-      builder: (context, snapshot) {
-        // Check for errors
-        if (snapshot.hasError) {
-          return Scaffold(backgroundColor: Colors.white,body: Center(child: Text('hasError'),),);
-        }
-        // Once complete, show your application
-        if (snapshot.connectionState == ConnectionState.done) {
-          return App();
-          //return Scaffold(backgroundColor: Colors.white,body: Center(child: Text('done'),),);
-        }
-        // Otherwise, show something whilst waiting for initialization to complete
-        return Scaffold(backgroundColor: Colors.white,body: Container(),);
-      },
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        DefaultCupertinoLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      locale: Locale('es', 'ES'),
+      supportedLocales: [
+        const Locale('es', 'ES'), // English
+        const Locale('en', 'US'), // English
+      ],
+      home: FutureBuilder(
+        // Initialize FlutterFire:
+        future: _initialization,
+        builder: (context, snapshot) {
+          // Check for errors
+          if (snapshot.hasError) {
+            return Scaffold(backgroundColor: Colors.white,body: Center(child: Text('hasError'),),);
+          }
+          // Once complete, show your application
+          if (snapshot.connectionState == ConnectionState.done) {
+            return MyApp();
+            //return Scaffold(backgroundColor: Colors.white,body: Center(child: Text('done'),),);
+          }
+          // Otherwise, show something whilst waiting for initialization to complete
+          return Scaffold(backgroundColor: Colors.white,body: Container(),);
+        },
+      ),
     );
   }
 }
