20c7c1da4c1e58c2f0851d1d619891a626a8aecc
jesusdja <jesusdja@smartdream.es>
2021-03-26 23:31:49 -0400

    agregar data a traduccion

--- lib/views/binnacle/binnacle.dart
+++ lib/views/binnacle/binnacle.dart
@@ -504,44 +504,39 @@ class Home extends StatelessWidget {
 class ListPage extends StatelessWidget {
   ListPage();
 
+  final ItemPositionsListener itemPositionsListener =
+  ItemPositionsListener.create();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text("List"),
         ),
-        body: Body());
-  }
-}
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            var position = itemPositionsListener.itemPositions.value.first.index;
+            print(position);
+          },
+        ),
+        body: WillPopScope(
+            onWillPop: () {
+              var position = itemPositionsListener.itemPositions.value.first.index;
+              print(position);
+              //trigger leaving and use own data
+              //Navigator.pop(context, false);
 
-class Body extends StatefulWidget {
-  @override
-  _BodyState createState() => _BodyState();
-}
-
-class _BodyState extends State<Body> {
-  _BodyState();
-
-  final ItemPositionsListener itemPositionsListener =
-  ItemPositionsListener.create();
-
-  @override
-  Widget build(BuildContext context) {
-    return WillPopScope(
-        onWillPop: () {
-          var position = itemPositionsListener.itemPositions.value.first.index;
-          print(position);
-          //trigger leaving and use own data
-          Navigator.pop(context, false);
-
-          //we need to return a future
-          return Future.value(false);
-        },
-        child: ScrollablePositionedList.builder(
-            initialScrollIndex: 400,
-            itemPositionsListener: itemPositionsListener,
-            itemCount: 500,
-            reverse: true,
-            itemBuilder: (context, index) => Text('Item $index')));
+              //we need to return a future
+              return Future.value(true);
+            },
+            child: ScrollablePositionedList.builder(
+                initialScrollIndex: 400,
+                itemPositionsListener: itemPositionsListener,
+                itemCount: 500,
+                reverse: true,
+                itemBuilder: (context, index) => Text('Item $index'),
+            ),
+        ),
+    );
   }
 }
